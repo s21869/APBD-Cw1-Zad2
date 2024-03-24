4f2ad3cab45a8f638c967488d5b2799325e20def
s21869 <s21869@pjwstk.edu.pl>
2024-03-24 23:08:21 +0100

    srednia z intow

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Mod3");
-Console.WriteLine("Bye, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 5, 10, 15, 20, 25 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine("Średnia wynosi: " + average);
+    }
+
+    static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Tablica liczb nie może być pusta.");
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        return (double)sum / numbers.Length;
+    }
+}
