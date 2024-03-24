954dd04f86feff7b4be3d6579cdb387ea70577b2
s21869 <s21869@pjwstk.edu.pl>
2024-03-24 22:52:22 +0100

    Modyfikacja 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World! Mod1");
+Console.WriteLine("Hello, World! Mod2");
+Console.WriteLine("Hello, World! Mod2");
